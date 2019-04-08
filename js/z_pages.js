69be984c02fa28d4944b788fd57c37db1b2fee34
zlxone <994473327@qq.com>
2019-04-08 17:57:44 +0800

    zpage++

--- js/z_pages.js
+++ js/z_pages.js
@@ -39,6 +39,26 @@
             };
         })
 
+        //轮播图
+        var mySwiper = new Swiper ('.swiper-container', {
+            // direction: 'vertical', // 垂直切换选项
+            loop: true, // 循环模式选项
+            autoplay:true,
+            // 如果需要分页器
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            // 如果需要前进后退按钮
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            // 如果需要滚动条
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+        })
+
         // 跑马灯
         for (let i = 0; i < $('.z_pmdwrap').length; i++) {
             let wrap = $('.z_pmdwrap')[i], first = $('.z_mpdfirst')[i];
